db462a9cd3db4887351b99778c0485faa4eda644
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-05-04 15:12:30 +0700

    fixed: refactor button component

--- src/components/buttons/OutlinedButton.vue
+++ src/components/buttons/OutlinedButton.vue
@@ -6,15 +6,12 @@ const props = defineProps<{
   click?: () => void
   to?: string | object
 }>()
+
+const baseClass =
+  'flex justify-center items-center w-[120px] h-[46px] border border-primary-orange rounded-sm text-primary-orange hover:bg-primary-orange duration-300 hover:text-white cursor-pointer'
 </script>
 
 <template>
-  <component
-    :is="props.to ? RouterLink : 'button'"
-    :to="props.to"
-    @click="props.to ? undefined : props.click"
-    class="flex justify-center items-center w-[120px] h-[46px] border border-primary-orange rounded-sm text-primary-orange hover:bg-primary-orange duration-300 hover:text-white cursor-pointer"
-  >
-    {{ props.name }}
-  </component>
+  <RouterLink v-if="props.to" :to="props.to" :class="baseClass">{{ props.name }}</RouterLink>
+  <button v-else @click="props.click" :class="baseClass">{{ props.name }}</button>
 </template>
--- src/components/buttons/PrimaryButton.vue
+++ src/components/buttons/PrimaryButton.vue
@@ -6,14 +6,12 @@ const props = defineProps<{
   click?: () => void
   to?: string | object
 }>()
+
+const baseClass =
+  'flex justify-center items-center w-[120px] h-[46px] text-sm xl:text-base rounded-sm bg-primary-orange text-white hover:opacity-75 duration-300 cursor-pointer'
 </script>
 
 <template>
-  <component
-    :is="props.to ? RouterLink : 'button'"
-    :to="props.to"
-    @click="props.to ? undefined : props.click"
-    class="flex justify-center items-center w-[120px] h-[46px] text-sm xl:text-base rounded-sm bg-primary-orange text-white hover:opacity-75 duration-300 cursor-pointer"
-    >{{ name }}</component
-  >
+  <RouterLink v-if="props.to" :to="props.to" :class="baseClass">{{ props.name }}</RouterLink>
+  <button v-else @click="props.click" :class="baseClass">{{ props.name }}</button>
 </template>
