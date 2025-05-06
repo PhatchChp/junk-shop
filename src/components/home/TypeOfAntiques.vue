e221daa8aa9d921a8a324c20e56a281dbfe1301f
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-05-06 23:37:12 +0700

    update: card for responsive screen

--- src/components/home/TypeOfAntiques.vue
+++ src/components/home/TypeOfAntiques.vue
@@ -11,8 +11,8 @@ const routeName = 'category'
   <div
     class="flex flex-col justify-center items-center gap-6 my-20 p-8 bg-primary-gray rounded-2xl"
   >
-    <h1 class="text-heading-48">ประเภทของเก่าที่เรารับซื้อ</h1>
-    <div class="grid grid-cols-4 gap-6">
+    <h1 class="text-heading-48 text-center">ประเภทของเก่าที่เรารับซื้อ</h1>
+    <div class="grid grid-col-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
       <div v-for="item in category.items" :key="item.nameEN">
         <RouterLink :to="{ name: routeName, params: { type: item.type } }">
           <CardImage :image-url="item.imageUrl" :title="item.nameTH" :subtitle="item.nameEN" />
